bd526f651203f71f10fc43408997e539652ec2a9
AkshayaKolhe <kolheakshaya25@gmail.com>
2024-07-23 18:08:32 +0530

    cloudfront added

--- route53.tf
+++ route53.tf
@@ -14,7 +14,7 @@ resource "aws_route53_record" "india_geolocation_record" {
 
   alias {
     name                   = aws_cloudfront_distribution.cloudfront.dns_name
-    zone_id                = aws_cloudfront_distribution.cloudfront.zone_id
+    zone_id                = "Z2FDTNDATAQYW2"
     evaluate_target_health = true
 
   }
