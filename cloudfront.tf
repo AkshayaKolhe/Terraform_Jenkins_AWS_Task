63821cfb2472e01023a551dcd33284e8ff6fdfb6
AkshayaKolhe <kolheakshaya25@gmail.com>
2024-07-23 18:01:56 +0530

    cloudfront added

--- cloudfront.tf
+++ cloudfront.tf
@@ -30,4 +30,8 @@ restrictions {
       restriction_type = "none"
     }
   }
+   viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
 }
